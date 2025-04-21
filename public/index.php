fa54b204e79f1f55c898cb755d9ce430c5cab745
VitorVts <vitorgguimaraes56@gmail.com>
2025-04-21 18:30:59 -0300

    Add:insert Cors into index.php

--- public/index.php
+++ public/index.php
@@ -1,8 +1,9 @@
 <?php
 
-header('Access-Control-Allow-Origin: *');
+header('Access-Control-Allow-Origin: http://localhost:5173');
 header('Access-Control-Allow-Methods: POST, OPTIONS');
 header('Access-Control-Allow-Headers: Content-Type');
+header('Access-Control-Allow-Credentials: true');
 
 if ($_SERVER['REQUEST_METHOD'] === 'OPTIONS') {
     http_response_code(204);
