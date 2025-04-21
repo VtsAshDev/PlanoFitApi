2fdd83c08565251c2c17fac294a42a5b322208a2
VitorVts <vitorgguimaraes56@gmail.com>
2025-04-21 19:30:00 -0300

    Add: Allow origin cors

--- public/index.php
+++ public/index.php
@@ -1,5 +1,7 @@
 <?php
 
+$allowed_origin = 'https://plano-fit.vercel.app';
+
 if (isset($_SERVER['HTTP_ORIGIN']) && $_SERVER['HTTP_ORIGIN'] === $allowed_origin) {
     header("Access-Control-Allow-Origin: $allowed_origin");
     header("Access-Control-Allow-Methods: GET, POST, PUT, DELETE, OPTIONS");
